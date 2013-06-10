f81e44e0d8508ac127522604daea99f90df07cba
ホクトの. <hokuto.lee@gmail.com>
2013-06-10 14:26:33 +0900

    Update csnuts.go

--- csnuts/csnuts.go
+++ csnuts/csnuts.go
@@ -16,8 +16,8 @@ import (
 )
 
 const lenSummery=1370
-//var Site string="http://go130610.appengine.google.com"
-var Site string="http://go130610.appengine.google.com"
+//var Site string="http://go130610.appspot.com"
+var Site string="http://go130610.appspot.com"
 var templatePath string="template/cn/"
 
 type TagCount struct {
